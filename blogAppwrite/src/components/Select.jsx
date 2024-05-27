bcf14a48d41351ac45760bf4eb555467c66b01ab
Mohit Mehto <osndl.mohitm@aicofindia.com>
2024-05-27 23:31:22 +0530

    changes

---
+++ blogAppwrite/src/components/Select.jsx
@@ -0,0 +1,28 @@
+import React, { forwardRef, useId } from "react";
+
+const Select = ({ options, className = "", label, ...props }, ref) => {
+  const id = useId();
+  return (
+    <div>
+      {label && (
+        <label htmlFor={id} className="${className}">
+          {label}
+        </label>
+      )}
+      <select
+        id={id}
+        {...props}
+        ref={ref}
+        className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
+      >
+        {options?.map((option) => (
+          <options key={option} value={option}>
+            {option}
+          </options>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default forwardRef(Select);
