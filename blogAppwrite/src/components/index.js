e6c523c5dc887254565eb6764618c3b7dacc6f9e
Mohit Mehto <osndl.mohitm@aicofindia.com>
2024-05-30 20:34:14 +0530

    cahnges

--- blogAppwrite/src/components/index.js
+++ blogAppwrite/src/components/index.js
@@ -5,6 +5,21 @@ import LogoutBtn from "./header/LogoutBtn";
 import Container from "./container/Container";
 import Button from "./Button";
 import RTE from "./RTE";
-import Signup from "../pages/Signup";
+import Signup from "./SignUp";
+import Login from "./Login";
+import PostCard from "./PostCard";
+import PostForm from "./post-form/PostForm";
 
-export { Header, Footer, Logo, LogoutBtn, Container, Button, RTE, Signup };
+export {
+  Header,
+  Footer,
+  Logo,
+  LogoutBtn,
+  Container,
+  Button,
+  RTE,
+  Signup,
+  Login,
+  PostCard,
+  PostForm,
+};
---
+++ blogAppwrite/src/pages/AllPost.jsx
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+import services from "../appwrite/Dbconfig";
+import { Container, PostCard } from "../components";
+
+const AllPost = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    services.getPosts([]).then((posts) => {
+      if (posts) {
+        setPosts(posts.documents);
+      }
+    });
+  }, []);
+  return (
+    <div className="w-full py-8">
+      <Container>
+        <div className="flex flex-wrap">
+          {posts.map((post) => (
+            <div className="p-2 w-1/4" key={post.$id}>
+              <PostCard key={posts.$id} post={post} />
+            </div>
+          ))}
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default AllPost;
